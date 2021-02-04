b697da441de90ab667c93db0360f8e597f89da5d
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2021-02-03 21:35:42 -0300

    updated eslint, added fonts

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
 
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
 import styled from "styled-components";
 
-const App = () => (
-  <>
-    <Wrapper>
-      <Title>Hello, Fellas!</Title>
-      <Content>This is the test screen.</Content>
-    </Wrapper>
-  </>
-);
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    "Quicksand-Regular": require("./assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Bold": require("./assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Light": require("./assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-Medium": require("./assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-SemiBold": require("./assets/fonts/Quicksand-SemiBold.ttf"),
+  });
 
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <Wrapper>
+        <Title>Hello, Fellas!</Title>
+        <Content>This is the test screen.</Content>
+      </Wrapper>
+    </>
+  );
+};
 const Wrapper = styled.SafeAreaView`
   flex-grow: 1;
   align-items: center;
@@ -22,11 +38,14 @@ const Title = styled.Text`
   color: #1c1c1c;
   font-size: 24px;
   margin-bottom: 8px;
+  font-family: Quicksand-Bold;
+  font-weight: 800;
 `;
 
 const Content = styled.Text`
   color: #2e2e2e;
   font-size: 16px;
+  font-family: Quicksand-Regular;
 `;
 
 export default App;
