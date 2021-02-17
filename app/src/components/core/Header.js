b20d3f03d870f8df0372c243952cd76ed529315e
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2021-02-17 01:06:28 -0300

    left button header support

--- app/src/components/core/Header.js
+++ app/src/components/core/Header.js
@@ -7,12 +7,23 @@ import { Ionicons } from "@expo/vector-icons";
 
 import { Colors, Typography, Spacing } from "../../config";
 
-export function Header({ title, rightButtons }) {
+export function Header({ title, rightButtons, leftButtons }) {
   /*
   rightButtons: [
     {
-      icon: str, 
-      onPress: function,
+      icon: [str], 
+      label: [str],
+      onPress: [function],
+      ...props
+    },
+    ...
+  ]
+
+  leftButtons: [
+    {
+      icon: [str], 
+      label: [str],
+      onPress: [function],
       ...props
     },
     ...
@@ -24,19 +35,53 @@ export function Header({ title, rightButtons }) {
   if (rightButtons) {
     rightButtonsComponent = (
       <HeaderRightButtons>
-        {rightButtons.map(({ icon, onPress, ...props }, index) => (
-          <DefaultTouchable onPress={onPress} key={index.toString()} {...props}>
-            <StyledHeaderIcon name={icon} />
-          </DefaultTouchable>
+        {rightButtons.map(({ icon, onPress, label, ...props }, index) => (
+          <HeaderIconTouchable
+            onPress={onPress}
+            key={index.toString()}
+            {...props}
+          >
+            {icon ? <StyledHeaderIcon name={icon} /> : null}
+            {label ? <HeaderIconLabel>{label}</HeaderIconLabel> : null}
+          </HeaderIconTouchable>
         ))}
       </HeaderRightButtons>
     );
   }
 
+  if (!leftButtons) {
+    return (
+      <HeaderContainer>
+        <HeaderContent>
+          <HeaderTitle>{title}</HeaderTitle>
+          {rightButtonsComponent}
+        </HeaderContent>
+      </HeaderContainer>
+    );
+  }
+
+  let leftButtonsComponent = (
+    <HeaderLeftButtons>
+      {leftButtons.map(({ icon, onPress, label, ...props }, index) => (
+        <HeaderIconTouchable
+          onPress={onPress}
+          key={index.toString()}
+          {...props}
+        >
+          <StyledHeaderIcon name={icon} />
+          {label ? <HeaderIconLabel>{label}</HeaderIconLabel> : null}
+        </HeaderIconTouchable>
+      ))}
+    </HeaderLeftButtons>
+  );
+
   return (
     <HeaderContainer>
       <HeaderContent>
-        <HeaderTitle>{title}</HeaderTitle>
+        <HeaderCenterWrapper>
+          <HeaderTitle>{title}</HeaderTitle>
+        </HeaderCenterWrapper>
+        {leftButtonsComponent}
         {rightButtonsComponent}
       </HeaderContent>
     </HeaderContainer>
@@ -67,7 +112,22 @@ export const HeaderTitle = styled.Text`
   color: ${Colors.primaryDark};
 `;
 
+export const HeaderCenterWrapper = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: ${Spacing.getSpacing(16)};
+  align-items: center;
+  justify-content: center;
+  z-index: -1;
+`;
+
 export const HeaderRightButtons = styled.View`
+  flex-direction: row;
+  margin-left: auto;
+`;
+
+export const HeaderLeftButtons = styled.View`
   flex-direction: row;
 `;
 
@@ -76,6 +136,17 @@ export const StyledHeaderIcon = styled(Ionicons).attrs({
   size: 24,
 })`
   margin-left: ${Spacing.getSpacing(8)};
+`;
+
+export const HeaderIconLabel = styled.Text`
+  font-family: ${Typography.FONT_FAMILIES.Quicksand.Regular};
+  font-size: ${Typography.FONT_SIZES.large};
+  color: ${Colors.primaryDark};
+`;
+
+export const HeaderIconTouchable = styled(DefaultTouchable)`
+  flex-direction: row;
+  align-items: center;
 `;
 
 export default Header;
