b64ac32123b6860238bf0dc4ef2ba54181787516
isacruzdora <isadoracruz387@gmail.com>
2021-03-15 15:04:36 -0300

    Incluindo campo de edição(com Rich Text) e barra de edição(Rich Toolbar), e algumas alterações no styles.js para testes

--- app/src/components/presentational/Anotacao/AnotacaoPresentational.js
+++ app/src/components/presentational/Anotacao/AnotacaoPresentational.js
@@ -1,16 +1,24 @@
-import React from "react";
-
-import { Ionicons } from "@expo/vector-icons";
-
-import { Colors } from "../../../config";
-
-import Header from "../../core/Header";
-import useDimensions from "../../hooks/useDimensions";
+import React, { useRef, useState } from "react";
+import styled from "styled-components/native";
 
 import {
-  Wrapper,
-} from "./styles.js";
+  RichEditor,
+  RichToolbar,
+} from "react-native-pell-rich-editor";
+import  {  WebView  }  from 'react-native-webview' ;
 
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../../config";
+import Header from "../../core/Header";
+import useDimensions from "../../hooks/useDimensions";
+import {
+  AutorAnotacao,
+  ContainerTitleAnotacao,
+  ContainerAutorAnotacao,
+  Title,
+  TitleAnotacao,
+  Wrapper
+} from "./styles.js";
 
 function AnotacaoPresentational({ notebooks, refreshing, onRefresh, openCaderno, goBack }) {
 
@@ -35,11 +43,36 @@ function AnotacaoPresentational({ notebooks, refreshing, onRefresh, openCaderno,
     ],
   };
 
+  const RichText = useRef(); //referencia ao componente RichEditor
+  const [article, setArticle] = useState("");
+
   return (
     <>
       <Wrapper>
         <Header {...headerButtons} />
+        <ContainerTitleAnotacao>
+          <TitleAnotacao>Aplicações do Eletromagnetismo</TitleAnotacao>
+          <ContainerAutorAnotacao>
+            <AutorAnotacao>Por: Ednaldo Pereira</AutorAnotacao>
+          </ContainerAutorAnotacao>
+        </ContainerTitleAnotacao>
 
+        <RichEditor
+            disabled={false}
+            ref={RichText}
+            containerStyle= {{backgroundColor:"black", borderColor:"black"}}
+            //placeholder={"Conteudo..."}
+            onChange={(text) => setArticle(text)}
+          />
+
+          <RichToolbar
+            editor={RichText}
+            disabled={false}
+            iconTint={"${Colors.primaryLight}"}
+            selectedIconTint={"${Colors.secondary}"}
+            disabledIconTint={"${Colors.primaryLight}"}
+            iconSize={30}
+          />
       </Wrapper>
     </>
   );
--- app/src/components/presentational/Anotacao/styles.js
+++ app/src/components/presentational/Anotacao/styles.js
@@ -16,3 +16,31 @@ export const Wrapper = styled.SafeAreaView`
   height: 100%;
   position: relative;
 `;
+
+/*Componentes Criados para teste do Titulo e Autoria que aparecem na Tela Anotacao*/
+
+//Containers
+
+export const ContainerTitleAnotacao = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 10%;
+`;
+
+export const ContainerAutorAnotacao = styled.View`
+  width: 100%;
+  height: 20%;
+`;
+
+//Contents
+
+export const TitleAnotacao = styled.Text`
+  flex-flow: row wrap;
+  width: 100%;
+`;
+
+export const AutorAnotacao = styled.Text`
+  flex-flow: row wrap;
+  width: 100%;
+`;
