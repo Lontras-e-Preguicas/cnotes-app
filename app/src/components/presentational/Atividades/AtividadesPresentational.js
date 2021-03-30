b87e78212d7d0f782935bc896300690836a71911
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2021-03-30 03:13:13 -0300

    styling tweaks on activity screen
    fixed spacings and nested scrolling

--- app/src/components/presentational/Atividades/AtividadesPresentational.js
+++ app/src/components/presentational/Atividades/AtividadesPresentational.js
@@ -12,85 +12,93 @@ import {
   DescNotification,
   ListNotification,
   TitleNotification,
-  ScrollList,
   EmptyListTitle,
   EmptyListText,
-  ModalButtonRow,
-  DeleteModalButton,
+  Wrapper,
+  ContentContainer,
+} from "./styles.js";
+
+import Modal, {
   CancelModalButton,
-  Wrapper } from "./styles.js";
+  ConfirmModalButtom,
+  ModalButtonRow,
+} from "../../core/Modal";
 
-import Modal from "../../core/Modal";
+const AtividadesPresentational = ({ notificacoes, submitDeletion }) => {
+  const rightButtons = [
+    {
+      icon: "mail",
+      onPress: () => {
+        setModalVisible(true);
+      },
+    },
+  ];
 
-const AtividadesPresentational = ({notificacoes, submitDeletion}) => {
+  const [modalVisible, setModalVisible] = useState(false);
 
-const rightButtons = [
-  {
-    icon: "mail",
-    onPress: ()=> {setModalVisible(true);},
-  },
-];
-
-const [modalVisible, setModalVisible] = useState(false);
-
-return(
-  <>
-    <Wrapper>
-      <Header title="Atividades recentes" rightButtons={rightButtons} />
-      <ScrollList>
-        <ListNotification
+  return (
+    <>
+      <Wrapper>
+        <Header title="Atividades recentes" rightButtons={rightButtons} />
+        <ContentContainer>
+          <ListNotification
             sections={notificacoes}
             ListEmptyComponent={EmptyList}
             keyExtractor={(item, index) => index.toString()}
-            renderItem= {({section:{desc,data}}) => (
+            renderItem={({ section: { desc, data } }) => (
               <ContainerNotification>
                 <AlignBallNotification>
-                  <BallNotification></BallNotification>
+                  <BallNotification />
                 </AlignBallNotification>
                 <ContainerInfoNotification>
                   <TitleNotification>{data}</TitleNotification>
                   <DescNotification>{desc}</DescNotification>
                 </ContainerInfoNotification>
-              </ContainerNotification>)
-              }
-            renderSectionHeader={({ section: { title } }) =>
-              (
-                <ContainerDateNotification>
-                  <DateNotification>{title}</DateNotification>
-                </ContainerDateNotification>
-              )}
+              </ContainerNotification>
+            )}
+            renderSectionHeader={({ section: { title } }) => (
+              <ContainerDateNotification>
+                <DateNotification>{title}</DateNotification>
+              </ContainerDateNotification>
+            )}
           />
-        </ScrollList>
+        </ContentContainer>
         <ModalDelete
-          submitDeletion = {submitDeletion}
+          submitDeletion={submitDeletion}
           modalVisible={modalVisible}
           close={() => setModalVisible(false)}
         />
       </Wrapper>
     </>
   );
-}
+};
 
 const EmptyList = () => (
   <>
     <EmptyListTitle>Sem notificações</EmptyListTitle>
-      <EmptyListText>
-        Não há atividades recentes para serem vizualizadas aqui.
-      </EmptyListText>
-   </>
- );
+    <EmptyListText>
+      Não há atividades recentes para serem vizualizadas aqui.
+    </EmptyListText>
+  </>
+);
 
-const ModalDelete = ({
-  modalVisible,
-  close,
-  submitDeletion,
-}) => {
-
+const ModalDelete = ({ modalVisible, close, submitDeletion }) => {
   return (
-    <Modal visible={modalVisible} close={close} title="Você deseja limpar as notificações?">
+    <Modal
+      visible={modalVisible}
+      close={close}
+      title="Você deseja limpar as notificações?"
+    >
       <ModalButtonRow>
         <CancelModalButton onPress={close}>Cancelar</CancelModalButton>
-        <DeleteModalButton onPress={() => {submitDeletion([]); close();}}> Confirmar</DeleteModalButton>
+        <ConfirmModalButtom
+          onPress={() => {
+            submitDeletion([]);
+            close();
+          }}
+        >
+          Confirmar
+        </ConfirmModalButtom>
       </ModalButtonRow>
     </Modal>
   );
--- app/src/components/presentational/Atividades/styles.js
+++ app/src/components/presentational/Atividades/styles.js
@@ -4,20 +4,18 @@ import styled from "styled-components";
 import { Colors, Typography, Spacing } from "../../../config";
 import Button from "../../core/Button";
 
-export const Wrapper = styled.SafeAreaView`
+export const Wrapper = styled.View`
   height: 100%;
   background-color: ${Colors.primaryLight};
 `;
 
-//Secoes e Listas
+export const ContentContainer = styled.View`
+  height: 100%;
+  padding: ${Spacing.getSpacing(8)} ${Spacing.getSpacing(16)};
+`;
 
 export const ListNotification = styled.SectionList`
   flex: 1;
-`;
-
-export const ScrollList = styled.ScrollView`
-  width: 100%;
-  height: 100%;
 `;
 
 export const ContainerDateNotification = styled.View`
@@ -28,76 +26,45 @@ export const ContainerDateNotification = styled.View`
 `;
 
 export const DateNotification = styled.Text`
-  background: ${Colors.primaryLight};
   font-family: ${Typography.FONT_FAMILIES.Quicksand.Regular};
   font-size: ${Typography.FONT_SIZES.medium};
-  padding: ${Spacing.getSpacing(20)};
-  margin-left: ${Spacing.getSpacing(50)};
+  padding: ${Spacing.getSpacing(8)};
 `;
 
 export const ContainerNotification = styled.View`
-  background: ${Colors.primaryLight};
   flex-direction: row;
+  margin-top: ${Spacing.getSpacing(8)};
+  margin-bottom: ${Spacing.getSpacing(10)};
 `;
 
 //Balls Notification
 
 export const AlignBallNotification = styled.View`
-  background: ${Colors.primaryLight};
   justify-content: center;
-  width: 20%;
 `;
 
 export const BallNotification = styled.View`
   background-color: ${Colors.secondary};
-  align-self: center;
-  width: 15px;
-  height: 15px;
-  borderRadius: 15px;
+  width: 12px;
+  height: 12px;
+  border-radius: 12px;
 `;
 
-//Conteudos
-
 export const ContainerInfoNotification = styled.View`
-  background: ${Colors.primaryLight};
   flex-grow: 1;
-  flex-basis: 0;
+  margin-left: ${Spacing.getSpacing(12)};
 `;
 
 export const TitleNotification = styled.Text`
-  background: ${Colors.primaryLight};
   flex-flow: row wrap;
   font-family: ${Typography.FONT_FAMILIES.Quicksand.Regular};
   font-size: ${Typography.FONT_SIZES.large};
-
 `;
 export const DescNotification = styled.Text`
-  background: ${Colors.primaryLight};
   flex-flow: row wrap;
   font-family: ${Typography.FONT_FAMILIES.Quicksand.Regular};
   font-size: ${Typography.FONT_SIZES.regular};
-
 `;
-
-//  Modal excluir notificacoes
-
-export const ModalButtonRow = styled.View`
-  flex-direction: row;
-  margin-top: ${Spacing.getSpacing(16)};
-`;
-
-export const DeleteModalButton = styled(Button).attrs({
-  fill: true,
-  color: Colors.tertiaryAlt,
-  textColor: Colors.primaryLight,
-})`
-  flex: 1;
-  margin: 0 ${Spacing.getSpacing(8)};
-`;
-
-export const CancelModalButton = styled(DeleteModalButton).attrs({
-  color: Colors.secondaryAlt,
-})``;
 
 // Empty List
 
