d7e8821bb7a4767a02dbcf58d869dec4ce643ca2
isacruzdora <isadoracruz387@gmail.com>
2021-03-30 20:35:24 -0300

    Integração Modal Avaliação

--- app/src/components/containers/Anotacao/AnotacaoContainer.js
+++ app/src/components/containers/Anotacao/AnotacaoContainer.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 
 import AnotacaoPresentational from "../../presentational/Anotacao";
 import { pathJoin } from "../../../utils/format";
+import Toast from "react-native-toast-message";
+import {
+  API_URLS,
+  authenticatedFetch,
+  extractFailureInfo,
+} from "../../../utils/api";
 
 function AnotacaoContainer({ navigation, route }) {
   const [data, setData] = useState([]);
@@ -12,8 +18,47 @@ function AnotacaoContainer({ navigation, route }) {
     });
   };
 
-function submitRating(ratingvalue) {
-  //    console.log(ratingvalue);
+//ratingValue => é um param que armazenena o valor da avaliacao
+  const submitRating = async (ratingValue) => {
+    try {
+      const payload = {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ratingValue,
+          note_group: route.params.id,
+        }),
+      };
+      const res = await authenticatedFetch(
+        `${API_URLS.toEvaluate}${route.params.id}/rating/`,
+        payload,
+      );
+
+      if (res.status === 204) {
+        Toast.show({
+          type: "success",
+          text1: "Anotação avaliada com sucesso",
+        });
+        navigation.goBack();
+      } else {
+        const fInfo = await extractFailureInfo(res);
+
+        if (fInfo.fail) {
+          Toast.show({
+            type: "error",
+            text1: fInfo.message,
+          });
+        }
+      }
+    } catch {
+      Toast.show({
+        type: "error",
+        text1: "Falha ao realizar requisição",
+      });
+    }
+//console.log(ratingValue);
   };
 
   const presentationalProps = {
